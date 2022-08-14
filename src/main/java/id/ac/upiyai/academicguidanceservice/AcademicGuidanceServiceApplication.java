4ecaaa1814f2f1d7144833a0dc26bd4d0845d072
Moh Rifyal Azmi <skripsirifyal@gmail.com>
2022-08-15 00:35:03 +0700

    Add config repo properties

--- src/main/java/id/ac/upiyai/academicguidanceservice/AcademicGuidanceServiceApplication.java
+++ src/main/java/id/ac/upiyai/academicguidanceservice/AcademicGuidanceServiceApplication.java
@@ -2,8 +2,10 @@ package id.ac.upiyai.academicguidanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AcademicGuidanceServiceApplication {
 
     public static void main(String[] args) {
