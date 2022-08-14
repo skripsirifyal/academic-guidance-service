4e682dacbc148c5637a0f8bdbef39fe826d4cd7c
Moh Rifyal Azmi <skripsirifyal@gmail.com>
2022-08-14 12:31:30 +0700

    Initial commit

---
+++ src/main/java/id/ac/upiyai/academicguidanceservice/AcademicGuidanceServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.upiyai.academicguidanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicGuidanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicGuidanceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/upiyai/academicguidanceservice/AcademicGuidanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.upiyai.academicguidanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicGuidanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
